9acd2a2e2dac2c4b1463d352c6805bf7c2cc5978
Fluffy <fluffy.l2032@gmail.com>
2018-06-04 00:46:37 +0200

    add pack animal filter
    resolves #33

---
+++ Source/BetterAnimalsTab/Filters/FilterWorker_PackAnimal.cs
@@ -0,0 +1,25 @@
+﻿// FilterWorker_CaravanCarryingCapacity.cs
+// Copyright Karel Kroeze, 2018-2018
+
+using RimWorld;
+using Verse;
+
+namespace AnimalTab
+{
+    public class FilterWorker_PackAnimal: FilterWorker
+    {
+        public override bool Allows( Pawn pawn )
+        {
+            var packAnimal = pawn.RaceProps.packAnimal;
+            switch ( State )
+            {
+                case FilterState.Inactive:
+                case FilterState.Exclusive when !packAnimal:
+                case FilterState.Inclusive when packAnimal:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
--- Source/BetterAnimalsTab/Properties/AssemblyInfo.cs
+++ Source/BetterAnimalsTab/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("18.2.1.27")]
-[assembly: AssemblyFileVersion("18.2.1.27")]
+[assembly: AssemblyVersion("18.2.1.29")]
+[assembly: AssemblyFileVersion("18.2.1.29")]
