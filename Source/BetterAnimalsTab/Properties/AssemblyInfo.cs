1c0790f5c541ea4b696af13710994591e0a3d2cb
Fluffy <fluffy.l2032@gmail.com>
2018-10-16 17:27:50 +0200

    'Release 3.0.105 [nolog]'

--- Source/BetterAnimalsTab/Properties/AssemblyInfo.cs
+++ Source/BetterAnimalsTab/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.15.102")]
-[assembly: AssemblyFileVersion("2.15.102")]
+[assembly: AssemblyVersion("3.0.105")]
+[assembly: AssemblyFileVersion("3.0.105")]
