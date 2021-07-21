99d99424405ad9f83eaba77a272bf8e217b9daf0
maarxx <chris.lewark@gmail.com>
2021-07-21 14:10:27 -0400

    Change Age Filter to Mimic Race Filter (#57)
    thank you!

--- Source/BetterAnimalsTab/Filters/FilterWorker_Age.cs
+++ Source/BetterAnimalsTab/Filters/FilterWorker_Age.cs
@@ -1,20 +1,73 @@
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using Verse;
 
 namespace AnimalTab
 {
     public class FilterWorker_Age : FilterWorker
     {
-        public override bool Allows( Pawn pawn )
-        {
-            if ( State == FilterState.Inactive )
-                return true;
+        DefMap<LifeStageDef, bool> allowed = new DefMap<LifeStageDef, bool>();
+        private IEnumerable<LifeStageDef> LifeStages => MainTabWindow_Animals.Instance.AllPawns.Select(p => p.ageTracker.CurLifeStage).Distinct();
 
-            var old = pawn.ageTracker.AgeBiologicalYearsFloat > pawn.RaceProps.lifeExpectancy * .9;
-            if ( State == FilterState.Inclusive && old )
+        public override FilterState State
+        {
+            get { return allowed.Values().Any(v => v) ? FilterState.Inclusive : FilterState.Inactive; }
+            set => throw new InvalidOperationException("FilterWorker_Age.set_State() should never be called");
+        }
+
+        public override bool Allows(Pawn pawn)
+        {
+            if (State == FilterState.Inactive)
                 return true;
-            if ( State == FilterState.Exclusive && !old )
-                return true;
+            return allowed[pawn.ageTracker.CurLifeStage];
+        }
+
+        public override void Clicked()
+        {
+            var options = new List<FloatMenuOption>();
+            options.Add(new FloatMenuOption("AnimalTab.All".Translate(), Deactivate));
+            foreach (var lifeStage in LifeStages)
+                options.Add(new FloatMenuOption_Persistent(lifeStage.LabelCap, () => Toggle(lifeStage), extraPartWidth: 30f, extraPartOnGUI: rect => DrawOptionExtra(rect, lifeStage)));
+
+            Find.WindowStack.Add(new FloatMenu(options));
+        }
+
+        public void Toggle(LifeStageDef lifeStage)
+        {
+            allowed[lifeStage] = !allowed[lifeStage];
+            MainTabWindow_Animals.Instance.Notify_PawnsChanged();
+        }
+
+        public void Deactivate()
+        {
+            allowed.SetAll(false);
+            MainTabWindow_Animals.Instance.Notify_PawnsChanged();
+        }
+
+        private bool DrawOptionExtra(Rect rect, LifeStageDef lifeStage)
+        {
+            if (State == FilterState.Inclusive && allowed[lifeStage])
+            {
+                Rect checkRect = new Rect(rect.xMax - rect.height + Constants.Margin, rect.yMin, rect.height, rect.height).ContractedBy(7f);
+                GUI.DrawTexture(checkRect, Widgets.CheckboxOnTex);
+            }
+
             return false;
+        }
+
+        public override string GetTooltip()
+        {
+            if (State == FilterState.Inactive)
+                return "AnimalTab.FilterInactiveTip".Translate();
+
+            var _allowed = new List<string>();
+            foreach (var lifeStage in LifeStages)
+                if (allowed[lifeStage])
+                    _allowed.Add(lifeStage.label);
+            return "AnimalTab.PawnKindFilterTip".Translate(_allowed.ToStringList("AnimalTab.Or".Translate()));
         }
     }
 }
